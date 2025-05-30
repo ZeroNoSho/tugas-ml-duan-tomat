83ab4512ceb885a0aebcdd40ee44ccad66a6f813
ZeroNoSho <inukuro351@gmail.com>
2025-05-30 22:20:51 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	deleted:    ui2/.gitignore
    	deleted:    ui2/README.md
    	deleted:    ui2/app.vue
    	deleted:    ui2/assets/css/main.css
    	deleted:    ui2/components/app/header.vue
    	deleted:    ui2/components/ui/Informasion.vue
    	deleted:    ui2/components/ui/InputFile.vue
    	deleted:    ui2/layouts/default.vue
    	deleted:    ui2/nuxt.config.ts
    	deleted:    ui2/package-lock.json
    	deleted:    ui2/package.json
    	deleted:    ui2/pages/index.vue
    	deleted:    ui2/public/favicon.ico
    	deleted:    ui2/public/robots.txt
    	deleted:    ui2/server/tsconfig.json
    	deleted:    ui2/tsconfig.json

--- ui2/app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="min-h-screen bg-gray-100">
-    <NuxtLayout>
-      <div class="min-h-screen bg-gray-100 p-4 justify-center flex flex-col">
-        <NuxtPage />
-      </div>
-    </NuxtLayout>
-  </div>
-</template>
--- ui2/components/app/header.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="flex justify-between items-center bg-white p-4 rounded shadow">
-    <div class="text-xl font-bold">LOGO</div>
-  </div>
-</template>
